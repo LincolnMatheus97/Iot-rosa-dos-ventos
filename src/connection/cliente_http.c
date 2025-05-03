f779c36c3d83bd2b8283f0cc69d2741cb3bbc768
LincolnMatheus97 <linconlmatheus25@gmail.com>
2025-05-03 19:13:35 -0300

    :truck: chore: Modifico o envio de dados

--- src/connection/cliente_http.c
+++ src/connection/cliente_http.c
@@ -31,12 +31,14 @@ static err_t callback_conectado(void *arg, struct tcp_pcb *pcb, err_t err) {
     tcp_recv(pcb, callback_resposta_recebida);
 
     // Coletar dados
+    uint8_t x = leitura_joystick_x();
+    uint8_t y = leitura_joystick_y();
     const char* direcao = verificar_movimento();
 
     char corpo_json[128];
     snprintf(corpo_json, sizeof(corpo_json),
-             "{\"direcao\": \"%s\"}",
-             direcao);
+             "{\"direcao\": %s, \"x\": %d, \"y\": %d}",
+             direcao, x, y);
 
     char requisicao[512];
     // Usar PROXY_HOST no cabeçalho Host
