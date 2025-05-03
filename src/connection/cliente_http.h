a3ea6b6f1ddad9b2f85a32464bb745eb4393c54d
LincolnMatheus97 <linconlmatheus25@gmail.com>
2025-05-03 18:04:05 -0300

    :sparkles: feat: Adiciono logica de conexao

---
+++ src/connection/cliente_http.h
@@ -0,0 +1,18 @@
+#ifndef CLIENTE_HTTP_H
+#define CLIENTE_HTTP_H
+
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include "pico/cyw43_arch.h"
+#include "lwip/dns.h"
+#include "lwip/ip_addr.h"
+#include "lwip/tcp.h"
+#include "src/utils/joystick.h"
+
+#define PROXY_HOST "maglev.proxy.rlwy.net"
+#define PROXY_PORT 48443
+
+void enviar_dados_para_nuvem();
+
+#endif
